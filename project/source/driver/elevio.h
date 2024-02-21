f50217328633b6d89fd3f7a449c4779933dfbf3d
Helene <heleness@stud.ntnu.no>
2024-02-21 17:52:59 +0100

    explained given code

--- project/source/driver/elevio.h
+++ project/source/driver/elevio.h
@@ -19,16 +19,26 @@ typedef enum {
 } ButtonType;
 
 
+//initializes elevator (use in the beginning)
 void elevio_init(void);
 
+//sets motor direction, keeps motor going that direction until called otherwise
 void elevio_motorDirection(MotorDirection dirn);
+//sets inputted buttonType in inputted floor to on if value 1, and turns light of value 0
 void elevio_buttonLamp(int floor, ButtonType button, int value);
+//sets floor indicator lamp of input
 void elevio_floorIndicator(int floor);
+//if input value 1: door light on, value 0: door light of
 void elevio_doorOpenLamp(int value);
+ //if value 1: turns on stop lamp, if value 0: turns of stop lamp
 void elevio_stopLamp(int value);
 
+//uses floor and buttontype, and returns 1 is buttontype is pressed in floor, 0 if not
 int elevio_callButton(int floor, ButtonType button);
+//returns current floor, and -1 if between floors
 int elevio_floorSensor(void);
+//returns 1 if stopButton pressed, 0 if not
 int elevio_stopButton(void);
+//returns 1 if obstruction, 0 if not
 int elevio_obstruction(void);
 
