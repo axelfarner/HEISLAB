35ea74a0e0fe7ff8555366a3d64abb3804b5e502
Oliver Hole Ekelund <oliver.hole.ekelund@hotmail.no>
2024-03-07 12:03:31 +0100

    Working test build

--- project/source/main.c
+++ project/source/main.c
@@ -2,54 +2,19 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <time.h>
-#include "driver/elevio.h"
 #include "driver/elevatorController.h"
+#include "driver/inputController.h"
 
 
 
+int main() {
 
-
-int main(){
     initializeElevator();
 
-    runElevator();
-
-
-
-    /* Cokmenting out in favour to runElevator();
-    while(1){
-        int floor = elevio_floorSensor();
-
-        if(floor == 0){
-            elevio_motorDirection(DIRN_UP);
-        }
-
-        if(floor == N_FLOORS-1){
-            elevio_motorDirection(DIRN_DOWN);
-        }
-
-
-        for(int f = 0; f < N_FLOORS; f++){
-            for(int b = 0; b < N_BUTTONS; b++){
-                int btnPressed = elevio_callButton(f, b);
-                elevio_buttonLamp(f, b, btnPressed);
-            }
-        }
-
-        if(elevio_obstruction()){
-            elevio_stopLamp(1);
-        } else {
-            elevio_stopLamp(0);
-        }
-        
-        if(elevio_stopButton()){
-            elevio_motorDirection(DIRN_STOP);
-            break;
-        }
-        
-        nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);    
+    while(19) {
+        processInput();
     }
-    */
 
+    // runElevator();
     return 0;
 }
